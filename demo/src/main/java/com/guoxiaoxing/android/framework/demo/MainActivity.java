3adc900ed6737a7fc5f4349cff6d493fe56f0814
guoxiaoxing <guoxiaoxing@souche.com>
2017-02-24 10:20:08 +0800

    initial commit

---
+++ demo/src/main/java/com/guoxiaoxing/android/framework/demo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.guoxiaoxing.android.framework.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_chapter1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_chapter1:
+                break;
+        }
+    }
+}
